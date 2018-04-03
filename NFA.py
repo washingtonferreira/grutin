56339aa2c696a9ddd929f079df8b1c893b0cacaa
washingtonferreira <washington-ferreira@hotmail.com.br>
2018-04-03 09:47:33 -0300

    Add files via upload
    Autômatos Finitos Não Determinísticos

---
+++ NFA.py
@@ -0,0 +1,37 @@
+import re
+from automata.fa.nfa import NFA
+
+defFormal = input('Informe a Definição Formal do AFND: ')
+defFormalPadrao = re.findall('\{.*?\}', defFormal)
+
+for i in range(len(defFormalPadrao)):
+    defFormalPadrao[i] = defFormalPadrao[i].replace('{', '').replace('}', '').replace(' ', '').split(',')
+
+estados = set(defFormalPadrao[0])
+alfabeto = set(defFormalPadrao[1])
+estInical = defFormalPadrao[2][0]
+estFinais = set(defFormalPadrao[3])
+funcaoTransicao = {}
+
+for e in estados:
+    transicaoEstado = {}
+
+    for a in alfabeto:
+        if a == '':
+            transicaoEstado[a] = input('Estado {} vendo {} vai para o Estado: '.format(e, 'vazio'))
+        else:
+            transicaoEstado[a] = input('Estado {} vendo {} vai para o Estado: '.format(e, a))
+    funcaoTransicao[e] = transicaoEstado
+
+cadeia = input("Informe a cadeia: ")
+
+nfa = NFA(
+    states=estados,
+    input_symbols=alfabeto,
+    transitions=funcaoTransicao,
+    initial_state=estInical,
+    final_states=estFinais
+)
+
+print(list(nfa.validate_input(cadeia, step=True)))
+
