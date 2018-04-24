196f9a77665f6f4c1a39f4789d0bbf83622a5f10
washingtonferreira <washington-ferreira@hotmail.com.br>
2018-04-24 18:52:05 -0300

    Classes modularizadas

--- automatos/MT.py
+++ automatos/MT.py
@@ -1,56 +1,66 @@
 import re
 from automata.tm.dtm import DTM
 
-regex  = '\{.*?\}'
-defFormal = input('Informe a Definição Formal da Máquina de Turing: ')
-defFormalPadrao = re.findall(regex, defFormal)
+regex = '\{.*?\}'
 
-for i in range(len(defFormalPadrao)):
-    defFormalPadrao[i] = defFormalPadrao[i].replace('{', '').replace('}', '').replace(' ', '').split(',')
 
-estados = defFormalPadrao[0]
-alfabeto = defFormalPadrao[1]
-simbolosFita = defFormalPadrao[2]
-estadoInicial = defFormalPadrao[3][0]
-simboloBranco = defFormalPadrao[4][0]
-estadosFinais = defFormalPadrao[5]
-funcaoTransicao = {}
+def definicao_formal(def_formal):
+    def_formal_padrao = re.findall(regex, def_formal)
 
-for e in estados:
-    transicaoEstado = {}
+    for i in range(len(def_formal_padrao)):
+        def_formal_padrao[i] = def_formal_padrao[i].replace('{', '').replace('}', '').replace(' ', '').split(',')
 
-    for s in simbolosFita:
-        transicao = input('({},{}): '.format(e, s))
+    estados = def_formal_padrao[0]
+    alfabeto = def_formal_padrao[1]
+    simbolos_fita = def_formal_padrao[2]
+    estado_inicial = def_formal_padrao[3][0]
+    simbolo_branco = def_formal_padrao[4][0]
+    estados_finais = def_formal_padrao[5]
 
-        if transicao != '':
-            transicaoEstado[s] = transicao.replace('(', '').replace(')', '').replace(' ', '').split(',')
-    if len(transicaoEstado) != 0:
-        funcaoTransicao[e] = transicaoEstado
+    return estados, alfabeto, simbolos_fita, estado_inicial, simbolo_branco, estados_finais
 
-#print(funcaoTransicao)
 
-dtm = DTM(
-    states=set(estados),
-    input_symbols=set(alfabeto),
-    tape_symbols=set(simbolosFita),
-    transitions=funcaoTransicao,
-    initial_state=estadoInicial,
-    blank_symbol=simboloBranco,
-    final_states=set(estadosFinais)
-)
+def cria_funcao_transicao(estados, simbolos_fita):
+    funcao_transicao = {}
 
-stringRecebida = input('Informe a string: ')
+    for e in estados:
+        transicaoEstado = {}
 
-resultado = ''
+        for s in simbolos_fita:
+            transicao = input('({},{}): '.format(e, s))
 
-for state, tape in dtm.validate_input(stringRecebida, step=True):
-    if resultado == '':
-        resultado = state
-        for t in tape.tape:
-            resultado += t
-    else:
-        resultado += ' |- ' + state
-        for t in tape.tape:
-            resultado += t
+            if transicao != '':
+                transicaoEstado[s] = transicao.replace('(', '').replace(')', '').replace(' ', '').split(',')
+        if len(transicaoEstado) != 0:
+            funcao_transicao[e] = transicaoEstado
 
-print(resultado)
+    return funcao_transicao
+
+
+def cria_maq_turing(estados, alfabeto, simbolos_fita, funcao_transicao, est_inicial, simbolo_branco, est_finais):
+    dtm = DTM(
+        states=set(estados),
+        input_symbols=set(alfabeto),
+        tape_symbols=set(simbolos_fita),
+        transitions=funcao_transicao,
+        initial_state=est_inicial,
+        blank_symbol=simbolo_branco,
+        final_states=set(est_finais)
+    )
+
+    return  dtm
+
+
+def retorna_derivacao(mt, string_recebida):
+    resultado = ''
+    for state, tape in mt.validate_input(string_recebida, step=True):
+        if resultado == '':
+            resultado = state
+            for t in tape.tape:
+                resultado += t
+        else:
+            resultado += ' |- ' + state
+            for t in tape.tape:
+                resultado += t
+
+    return resultado
--- automatos/NFAtoDFA.py
+++ automatos/NFAtoDFA.py
@@ -2,43 +2,34 @@ import re
 from automata.fa.nfa import NFA
 from automata.fa.dfa import DFA
 
-defFormal = input('Informe a Definição Formal do AFND: ')
-defFormalPadrao = re.findall('\{.*?\}', defFormal)
 
-for i in range(len(defFormalPadrao)):
-    defFormalPadrao[i] = defFormalPadrao[i].replace('{', '').replace('}', '').replace(' ', '').split(',')
+def definicao_formal(def_formal):
+    def_formal_padrao = re.findall('\{.*?\}', def_formal)
 
-estados = defFormalPadrao[0]
-alfabeto = defFormalPadrao[1]
-estInical = defFormalPadrao[2][0]
-estFinais = defFormalPadrao[3]
-funcaoTransicao = {}
+    for i in range(len(def_formal_padrao)):
+        def_formal_padrao[i] = def_formal_padrao[i].replace('{', '').replace('}', '').replace(' ', '').split(',')
 
-for e in estados:
-    transicaoEstado = {}
+    estados = def_formal_padrao[0]
+    alfabeto = def_formal_padrao[1]
+    estado_inical = def_formal_padrao[2][0]
+    estados_finais = def_formal_padrao[3]
 
-    for a in alfabeto:
-        if a == '':
-            transicaoEstado[a] = input('Estado {} vendo {} vai para o Estado: '.format(e, 'vazio')).split(',')
-        else:
-            transicaoEstado[a] = input('Estado {} vendo {} vai para o Estado: '.format(e, a)).split(',')
-        for t in transicaoEstado[a]:
-            if t == '':
-                transicaoEstado.pop(t)
-    funcaoTransicao[e] = transicaoEstado
+    return estados, alfabeto, estado_inical, estados_finais
 
-nfa = NFA(
-    states=set(estados),
-    input_symbols=set(alfabeto),
-    transitions=funcaoTransicao,
-    initial_state=estInical,
-    final_states=set(estFinais)
-)
 
-dfa = DFA(nfa)
+def converter_nfa_dfa(estados, alfabeto, funcao_transicao, est_inicial, est_finais):
+    nfa = NFA(
+        states=set(estados),
+        input_symbols=set(alfabeto),
+        transitions=funcao_transicao,
+        initial_state=est_inicial,
+        final_states=set(est_finais)
+    )
 
-print('Automato Finito Deterministico gerado: {}'.format(dfa.states))
+    dfa = DFA(nfa)
 
-cadeia = input("Informe a cadeia: ")
+    return dfa
 
-print(list(dfa.validate_input(cadeia, step=True)))
+
+def retorna_estados(nfa, cadeia):
+    print(list(nfa.validate_input(cadeia, step=True)))
